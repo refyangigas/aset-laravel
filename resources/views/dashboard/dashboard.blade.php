7de423a7c541648fd22f29b405731c4664ded859
refyangigas <119940877+refyangigas@users.noreply.github.com>
2024-01-04 14:40:38 +0700

    Update Dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -73,7 +73,7 @@
                                             </div>
                                             <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                                 <h6 class="text-muted font-semibold">Kategori</h6>
-                                                <h6 class="font-extrabold mb-0">4</h6>
+                                                <h6 class="font-extrabold mb-0">5</h6>
                                             </div>
                                         </div>
                                     </div>
